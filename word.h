9840a962b013c7592f245f40fe2742d040e8ec3f
Cindy Wong <cindy.wong93@hotmail.com>
2015-06-03 06:48:28 -0400

    Update word.h

--- word.h
+++ word.h
@@ -35,3 +35,91 @@ private:
 };
 
 #endif
+/////////////
+/*******************************************************
+H E A D E R  F I L E
+******************************************************/
+
+//*****************************************************************************
+//
+//  Name:  Wein T. Wong
+//	Z#:  Z23320315
+//	Instructor:  Dr. Bullard
+//	Class:  Data Structures
+//	Term:  Summer 2015
+//	Assignment #2 (singly linked list)
+//	Due Date:  June 3, 2015   
+//	Due Time:  10:00PM
+//	Points:  100 
+//	
+//  Description: Implement the WORD abstract data type using a singly-linked list of characters.
+//				 
+//******************************************************************************
+
+#ifndef WORD_H
+#define WORD_H
+
+#include <iostream>
+#include <string>
+#pragma once
+
+#pragma once
+#include <iostream>
+#include <string>
+
+
+class character
+{
+public:
+	char symbol;
+	character *next;
+};
+
+
+class WORD
+{
+public:
+
+	// Structors
+	WORD();
+	WORD(std::string);
+	WORD(const WORD & w);
+	~WORD();
+
+	// Operator overloads
+	WORD & operator=(const std::string &);
+	WORD & operator=(const WORD &w);
+	// Friends
+	friend std::ostream & operator<<(std::ostream &, WORD &);
+
+	// Behaviors
+//	void CatWords(WORD &);
+	int  Length();
+//	void Print();
+//	void Insert(WORD, int);
+	bool IsEmpty();
+	bool IsEqual(WORD &);
+	bool Remove(WORD &);
+	void RemoveALL(WORD &);
+//	void SetEqual(WORD &);
+
+	// Getter functions
+	character * getFront() { return front; }  // returns the address to the front node
+
+private:
+
+	// Helper functions
+	//character * Search(WORD &); // returns a character address to a matched word
+	void Search(character*&, character*&, WORD &);
+	character * SearchPrior(WORD &); // searches for what kind of mischief the node has been up to, then returns the node prior to a matched word
+//	void InsertLetter(char, int);    // creates a new node in memory and inserts it into the middle of the list
+//	void InsertLetter(char); // creates a new node in the front of a list
+	void add(char); // adds a character to the end of a list
+
+	// state variables 
+	character *front;  // front node in the list
+	character *back;   // back node in the list
+
+};
+
+#endif
